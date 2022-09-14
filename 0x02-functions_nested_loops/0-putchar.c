ffac09969824344030eb1aefe4957837ec258bb0
ameng000 <g.aman.h000@gmail.com>
2022-09-14 06:29:56 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * main - prints the string "_putchar" from a character array.
+ * main - prints putchar
  *
- * Return: 0 on success.
+ * Return: always 0
  *
  */
 
 int main(void)
 
 {
-_putchar("_");
-_putchar("p");
-_putchar("u");
-_putchar("t");
-_putchar("c");
-_putchar("h");
-_putchar("a");
-_putchar("r");
-_putchar("\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
