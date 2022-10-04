ef4e14ab331e2e9e6dbebc4bde1f4ee9bc01959b
ameng000 <g.aman.h000@gmail.com>
2022-10-04 07:35:15 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,50 +1,26 @@
-#include <main.h>
+#include "main.h"
+
 #include <stdio.h>
+
 #include <stdlib.h>
 
+#include <limits.h>
+
 /**
- * string_nconcat - function to concatnate strings with n bytes
- * @s1: destination for concatnation
+ * *malloc_checked - allocate memory with malloc
  *
- * @s2: source of string
+ * @b: unsigned int type
  *
- * @n: int type for size of byte
- *
- * Return: pointer to new memory allocated
+ * Return: return pointer
  *
  */
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *malloc_checked(unsigned int b)
 {
-int count, count1;
-int sign = n;
-char *ptr;
-int len1, len2;
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-for (len1 = 0; s1[len1] != '\0'; len1++)
-;
-for (len2 = 0; s2[len2] != '\0'; len2++)
-;
-if (sign >= len2)
-{
-sign = len2;
-ptr = malloc(sizeof(char) * (len1 + len2 + 1));
-}
-else
-ptr = malloc(sizeof(char) * (len1 + n + 1));
+int *ptr;
+ptr = malloc(b);
 if (ptr == NULL)
-return (NULL);
-for (count = 0; count < len1; count++)
-{
-ptr[count] = s1[count];
-}
-for (count1 = 0; count1 < sign; count1++)
-{
-ptr[count++] = s2[count1];
-}
-ptr[count++] = '\0';
+exit(98);
 return (ptr);
 }
+
